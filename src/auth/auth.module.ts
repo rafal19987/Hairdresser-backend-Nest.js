0bd6bcb5ced4a6f7b9d8c5c9981e83cf8b43a915
Rafixx <rafal987.1@o2.pl>
2025-05-11 23:05:02 +0200

    Refactor AuthService to use UserRepository and inject role service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
@@ -8,12 +8,14 @@ import { jwtConstants } from './constatns';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RefreshToken } from './entities/refresh-token.entity';
 import { RevokedToken } from './entities/revoked-token.entity';
+import { User } from '../users/entities/user.entity';
+import { Role } from '../roles/entities/role.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([RevokedToken, RefreshToken]),
-    UsersModule,
-    RolesModule,
+    TypeOrmModule.forFeature([RevokedToken, RefreshToken, User, Role]),
+    forwardRef(() => UsersModule),
+    forwardRef(() => RolesModule),
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
