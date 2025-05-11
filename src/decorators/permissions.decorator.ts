8860ace3106822edda6c54afaaacf96137a018f8
Rafixx <rafal987.1@o2.pl>
2025-05-11 23:05:18 +0200

    Update Permissions decorator to use PermissionDto type

--- src/decorators/permissions.decorator.ts
+++ src/decorators/permissions.decorator.ts
@@ -1,7 +1,7 @@
 import { SetMetadata } from '@nestjs/common';
-import { Permission } from 'src/roles/entities/role.entity';
+import { PermissionDto } from 'src/roles/entities/role.entity';
 
 export const PERMISSIONS_KEY = 'permissions';
 
-export const Permissions = (permissions: Permission[]) =>
+export const Permissions = (permissions: PermissionDto[]) =>
   SetMetadata(PERMISSIONS_KEY, permissions);
