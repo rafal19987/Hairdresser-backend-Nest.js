ffa01028914ff28c680b24685ccab70c6fd16095
Rafix <rafal987.1@o2.pl>
2026-02-21 14:14:36 +0100

    fix: export JwtModule from AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,26 +4,33 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { RolesModule } from 'src/roles/roles.module';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RefreshToken } from './entities/refresh-token.entity';
 import { RevokedToken } from './entities/revoked-token.entity';
 import { User } from '@/users/entities/user.entity';
 import { Role } from '@/roles/entities/role.entity';
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {StringValue} from "ms";
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([RevokedToken, RefreshToken, User, Role]),
     forwardRef(() => UsersModule),
     forwardRef(() => RolesModule),
-    JwtModule.register({
-        global: true,
-        secret: jwtConstants.accessTokenSecret,
-        signOptions: {expiresIn: jwtConstants.accessTokenExpiresIn},
+      JwtModule.registerAsync({
+          imports: [ConfigModule],
+          useFactory: async (configService: ConfigService) => ({
+              global: true,
+              secret: configService.get<string>('JWT_ACCESS_TOKEN_SECRET_KEY'),
+              signOptions: {
+                  expiresIn: configService.get<StringValue>('JWT_ACCESS_TOKEN_EXPIRES_IN'),
+              },
+          }),
+          inject: [ConfigService],
     }),
   ],
   providers: [AuthService],
   controllers: [AuthController],
-  exports: [AuthService],
+    exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
